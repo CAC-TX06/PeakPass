be018f3f53010574badf5f8ad6cfa9a794cd6881
Parker M <parker@pkrm.dev>
2023-03-04 23:50:00 -0600

    For sqlite hosting port

--- website/peakpass.py
+++ website/peakpass.py
@@ -5,15 +5,51 @@ from flask_login import current_user, login_user, login_required, logout_user
 from flask import redirect, render_template, request, url_for
 from func.login import correct_login_information
 from func.sign_up import add_user, hash_new_pass
-from __init__ import create_website
-from __init__ import User
 import sqlite3
 import bcrypt
 from cryptography.fernet import Fernet
 import hashlib
+from flask import Flask
+import flask_login
+from flask_login import UserMixin
+import random
 
-# Create the actual flask app (imported from __init__.py)
-app = create_website()
+class User(UserMixin):
+    pass
+
+class Password():
+    pass
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = str(random.randint(0, 10))
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+conn = sqlite3.connect("data.db")
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS users (email VARCHAR(100) PRIMARY KEY, password CHAR(60) NOT NULL)")
+cur.execute("CREATE TABLE IF NOT EXISTS passwords (id INTEGER PRIMARY KEY, owner VARCHAR(100), name TEXT NOT NULL, username TEXT, password TEXT, hash TEXT, url TEXT)")
+
+conn.commit()
+conn.close()
+
+@login_manager.user_loader
+def load_user(email):
+    conn = sqlite3.connect("data.db")
+    cur = conn.cursor()
+
+    # Check to see if the email is in the database
+    cur.execute("SELECT email FROM users WHERE email = ?", (email,))
+    email = cur.fetchone()
+    conn.close()
+
+    if email:
+        user = User()
+        user.id = email[0]
+        return user
+
 
 user_keys = {} # "email": "password"
 
